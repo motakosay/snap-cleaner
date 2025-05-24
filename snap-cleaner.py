9540f902720c02deeff118f6096b3e1be23e9dc9
Ironside <93154332+motakosay@users.noreply.github.com>
2025-05-24 04:20:52 +0300

    Update snap-cleaner.py

--- snap-cleaner.py
+++ snap-cleaner.py
@@ -7,7 +7,7 @@ import sys
 
 def remove_rec(im, y1, y2, alpha):
     pixels = im.load() #is used to access the pixel data of an image that has already been opened.
-    for i in range(im.size[0]):
+    for i in range(im.size[0]): #You are iterating over every horizontal position (column) in the image. This allows you to access or process every pixel along each row at that horizontal position.
         for j in range(y1, y2+1):
             new_color = tuple([int(v/(1-alpha)) for v in pixels[i,j]])
             pixels[i,j]=new_color
